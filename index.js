70923afb48ad1e8d77355ec961f77e787f3a5884
Gilmar Xavier de Oliveira <gilmarxo@gmail.com>
2020-09-02 22:27:06 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = 'localhost'; //127.0.0.1
 
 const port = 3000;
 
 const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('');
 
+   fs.readFile('index.html', (erro, data)=>{
+       if(erro){
+           res.writeHead(400, {'Content-Type':'text/plain'});
+           res.end('Página não encontrada!');
+
+       }else{
+           res.writeHead(200, {'Content-Type': 'text/html'});
+           res.write(data);
+           return res.end();
+       }
+   })  
 });
 
 server.listen(port, hostname, ()=> console.log(`Servidor rodando em: http://${hostname}:${port}`))
